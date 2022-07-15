bece689303df77e1a707ff30c949a0da19dd0e2c
Will <werobinson6@gmail.com>
2022-07-15 15:12:16 +0200

    clean up and format

--- lua/figure_numbers.lua
+++ lua/figure_numbers.lua
@@ -11,78 +11,78 @@ local seen_elements = {}
 local figure_numbers = {}
 
 local function table_length(t)
-    -- get the length of a table
-    local counter = 0
-    for v in pairs(t) do
-        counter = counter + 1
-    end
-    return counter
+  -- get the length of a table
+  local counter = 0
+  for v in pairs(t) do
+    counter = counter + 1
+  end
+  return counter
 end
 
 local function in_table(t, thing)
-    -- check if thing is in table t
-    for i, p in pairs(t) do
-        if p == thing then
-            return true
-        end
+  -- check if thing is in table t
+  for i, p in pairs(t) do
+    if p == thing then
+      return true
     end
-    return false
+  end
+  return false
 end
 
 local function get_figure_number(fig_table, id)
-    -- get the number of the figure with id
-    for i, p in pairs(fig_table) do
-        for a, b in pairs(p) do
-            if a == id then
-                replacement = tostring(b)
-                return replacement
-            end
-        end
+  -- get the number of the figure with id
+  for i, p in pairs(fig_table) do
+    for a, b in pairs(p) do
+      if a == id then
+        replacement = tostring(b)
+        return replacement
+      end
     end
-    return nil
+  end
+  return nil
 end
 
 local function replace_tag(pandocStr, type_tag, id, figure_numbers)
-    replacement = get_figure_number(figure_numbers[type_tag], id)
+  replacement = get_figure_number(figure_numbers[type_tag], id)
 
-    numbered_tag = pandocStr.text:gsub(patt, replacement)
+  numbered_tag = pandocStr.text:gsub(patt, replacement)
 
-    return numbered_tag
+  return numbered_tag
 end
 
 function Str(s)
-    if s.text:match(patt) then
-        local type_tag = s.text:match(type_patt)
-        local id = s.text:match(id_patt)
-        local replacement = "0"
+  if s.text:match(patt) then
+    local type_tag = s.text:match(type_patt)
+    local id = s.text:match(id_patt)
+    local replacement = "0"
 
-        if in_table(seen_elements, id) then
-            replacement = replace_tag(s, type_tag, id, figure_numbers)
-            return pandoc.Str(replacement)
-        else
-            table.insert(seen_elements, id)
-            if figure_numbers[type_tag] ~= nil then
-                local num_elems = table_length(figure_numbers[type_tag])
-                local insert = {}
-
-                insert[id] = num_elems + 1
-                table.insert(figure_numbers[type_tag], insert)
-            else
-                figure_numbers[type_tag] = {}
-                local insert = {}
-                insert[id] = 1
-                table.insert(figure_numbers[type_tag], insert)
-            end
-
-            replacement = replace_tag(s, type_tag, id, figure_numbers)
-
-            return pandoc.Str(replacement)
-        end
+    if in_table(seen_elements, id) then
+      replacement = replace_tag(s, type_tag, id, figure_numbers)
+      return pandoc.Str(replacement)
     else
-        return s
+      table.insert(seen_elements, id)
+      if figure_numbers[type_tag] ~= nil then
+        local num_elems = table_length(figure_numbers[type_tag])
+        local insert = {}
+
+        insert[id] = num_elems + 1
+        table.insert(figure_numbers[type_tag], insert)
+      else
+        figure_numbers[type_tag] = {}
+        local insert = {}
+        insert[id] = 1
+        table.insert(figure_numbers[type_tag], insert)
+      end
+
+      replacement = replace_tag(s, type_tag, id, figure_numbers)
+
+      return pandoc.Str(replacement)
     end
+  else
+    return s
+  end
 end
 
 return {
-    { Str = Str },
+  { Str = Str },
 }
